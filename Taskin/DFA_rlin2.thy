9e05882920ac59c6c52c60124f780c3cf7bee5e1
kaantaskin <ge87qeq@mytum.de>
2024-12-15 19:54:53 +0100

    final lemma proved

--- Taskin/DFA_rlin2.thy
+++ Taskin/DFA_rlin2.thy
@@ -12,6 +12,30 @@ definition nxt_rlin2_set :: "('n,'t)Prods \<Rightarrow> 'n set \<Rightarrow> 't 
 definition nxts_rlin2_set :: "('n,'t)Prods \<Rightarrow> 'n set \<Rightarrow> 't list \<Rightarrow> 'n set" where
 "nxts_rlin2_set P = foldl (nxt_rlin2_set P)"
 
+lemma map_Tm_single_nt:
+  "map Tm w @ [Tm a, Nt A] = u1 @ [Nt B] @ u2 \<Longrightarrow> u1 = map Tm (w @ [a]) \<and> u2 = []"
+proof -
+  assume "map Tm w @ [Tm a, Nt A] = u1 @ [Nt B] @ u2"
+  hence asm: "map Tm (w @ [a]) @ [Nt A] = u1 @ [Nt B] @ u2" by simp
+  have 1: "Nt B \<notin> set (map Tm (w @ [a]))" by auto
+  have 2: "Nt B \<in> set (u1 @ [Nt B] @ u2)" by simp
+  from asm 1 2 have "Nt B \<in> set ([Nt A])"
+    by (metis list.set_intros(1) rotate1.simps(2) set_ConsD set_rotate1 sym.inject(1))
+  hence "[Nt B] = [Nt A]" by simp
+  with 1 asm show ?thesis
+    by (metis append_Cons append_Cons_eq_iff append_self_conv emptyE empty_set)
+qed
+
+lemma map_Tm_not_eps:
+  "map Tm xs @ [Nt B] = map Tm wa @ \<alpha> \<Longrightarrow> \<alpha> \<noteq> []"
+proof
+  assume "map Tm xs @ [Nt B] = map Tm wa @ \<alpha>" "\<alpha> = []"
+  hence "map Tm wa = map Tm xs @ [Nt B]" by simp
+  hence "\<exists>xs' ys'. wa = xs' @ ys' \<and> map Tm xs' = map Tm xs \<and> map Tm ys' = [Nt B]"
+    by (simp add: Cons_eq_map_conv map_eq_append_conv)
+  thus False by blast
+qed
+
 lemma nxts_to_mult_derive:
   "B \<in> nxts_rlin2_set P M w \<Longrightarrow> (\<exists>A\<in>M. P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt B])"
 unfolding nxts_rlin2_set_def proof (induction w arbitrary: B rule: rev_induct)
@@ -35,7 +59,77 @@ next
   with A_in show ?case by blast
 qed
 
-lemma rlin2_nts_eq: 
+lemma rlin2_derive:
+  assumes "P \<turnstile> v1 \<Rightarrow>* v2" 
+      and "v1 = [Nt A]"
+      and "v2 = u1 @ [Nt B] @ u2" 
+      and "rlin2 P"
+    shows "\<exists>w. u1 = map Tm w \<and> u2 = []"
+using assms proof (induction arbitrary: u1 B u2 rule: rtrancl_derive_induct)
+  case base
+  then show ?case
+    by (simp add: append_eq_Cons_conv)
+next
+  case (step u C v w)
+  from step.prems(1) step.prems(3) have "\<exists>w. u = map Tm w \<and> v = []" 
+    using step.IH[of u C v] by simp
+  then obtain wh where u_def: "u = map Tm wh" by blast
+  have v_eps: "v = []"
+    using \<open>\<exists>w. u = map Tm w \<and> v = []\<close> by simp
+  from step.hyps(2) step.prems(3) have w_cases: "w = [] \<or> (\<exists>d D. w = [Tm d, Nt D])"
+    unfolding rlin2_def by auto
+  then show ?case proof cases
+    assume "w=[]"
+    with v_eps step.prems(2) have "u = u1 @ [Nt B] @ u2" by simp
+    with u_def show ?thesis by simp (metis ex_map_conv in_set_conv_decomp sym.distinct(1))
+  next
+    assume "\<not>w=[]"
+    then obtain d D where "w = [Tm d, Nt D]" 
+      using w_cases by blast
+    with u_def v_eps step.prems(2) have "u1 = map Tm (wh @ [d]) \<and> u2 = []"
+      using map_Tm_single_nt[of wh d D u1 B u2] by simp
+    thus ?thesis by blast
+  qed
+qed
+
+lemma rlin2_introduce_tm:
+  assumes "rlin2 P"
+      and "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Tm x, Nt B]"
+    shows "\<exists>C. P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt C] \<and> (C,[Tm x, Nt B]) \<in> P"
+proof -
+  from assms(2) have "\<exists>v. P \<turnstile> [Nt A] \<Rightarrow>* v \<and> P \<turnstile> v \<Rightarrow> map Tm w @ [Tm x, Nt B]"
+    using rtranclp.cases by fastforce
+  then obtain v where v_star: "P \<turnstile> [Nt A] \<Rightarrow>* v" and v_step: "P \<turnstile> v \<Rightarrow> map Tm w @ [Tm x, Nt B]" by blast
+  from v_step have "\<exists>u1 u2 C \<alpha>. v = u1 @ [Nt C] @ u2 \<and> map Tm w @ [Tm x, Nt B] = u1 @ \<alpha> @ u2 \<and> (C,\<alpha>) \<in> P"
+    using derive.cases by fastforce
+  then obtain u1 u2 C \<alpha> where v_def: "v = u1 @ [Nt C] @ u2" and w_def: "map Tm w @ [Tm x, Nt B] = u1 @ \<alpha> @ u2" 
+                          and C_prod: "(C,\<alpha>) \<in> P" by blast
+  from assms(1) v_star v_def have u2_eps: "u2 = []"
+    using rlin2_derive[of P "[Nt A]"] by simp
+  from assms(1) v_star v_def obtain wa where u1_def: "u1 = map Tm wa"
+    using rlin2_derive[of P "[Nt A]" "u1 @ [Nt C] @ u2" A u1] by auto
+  from w_def u2_eps u1_def have "map Tm w @ [Tm x, Nt B] = map Tm wa @ \<alpha>" by simp
+  then have "map Tm (w @ [x]) @ [Nt B] = map Tm wa @ \<alpha>" by simp
+  then have "\<alpha> \<noteq> []" 
+    using map_Tm_not_eps[of "w @ [x]" B wa \<alpha>] by simp
+  with assms(1) C_prod obtain d D where "\<alpha> = [Tm d, Nt D]"
+    using rlin2_def by fastforce
+  from w_def u2_eps have x_d: "x = d" 
+    using \<open>\<alpha> = [Tm d, Nt D]\<close> by simp
+  from w_def u2_eps have B_D: "B = D"
+    using \<open>\<alpha> = [Tm d, Nt D]\<close> by simp
+  from x_d B_D have alpha_def: "\<alpha> = [Tm x, Nt B]"
+    using \<open>\<alpha> = [Tm d, Nt D]\<close> by simp
+  from w_def u2_eps alpha_def have "map Tm w = u1" by simp
+  with u1_def have w_eq_wa: "w = wa"
+    by (metis list.inj_map_strong sym.inject(2))
+  from v_def u1_def w_eq_wa u2_eps have "v = map Tm w @ [Nt C]" by simp
+  with v_star have 1: "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt C]" by simp
+  from C_prod alpha_def have 2: "(C,[Tm x, Nt B]) \<in> P" by simp
+  from 1 2 show ?thesis by auto
+qed
+
+lemma rlin2_nts_derive_eq: 
   assumes "rlin2 P"
       and "P \<turnstile> [Nt A] \<Rightarrow>* [Nt B]"
     shows "A = B"
@@ -60,12 +154,6 @@ proof -
   qed simp
 qed
 
-lemma rlin2_intro_tm:
-  assumes "rlin2 P"
-      and "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Tm x, Nt B]"
-    shows "\<exists>C. P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt C] \<and> (C,[Tm x, Nt B]) \<in> P"
-  sorry
-
 lemma mult_derive_to_nxts:
   assumes "rlin2 P"
   shows "(\<exists>A\<in>M. P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt B]) \<Longrightarrow> B \<in> nxts_rlin2_set P M w"
@@ -73,7 +161,7 @@ unfolding nxts_rlin2_set_def using assms proof (induction w arbitrary: B rule: r
   case Nil
   then obtain A where A_unit: "P \<turnstile> [Nt A] \<Rightarrow>* [Nt B]" and A_in: "A \<in> M" by auto
   from A_unit Nil.prems(2) have "A = B"
-    using rlin2_nts_eq[of P A B] by simp
+    using rlin2_nts_derive_eq[of P A B] by simp
   with A_in show ?case by simp
 next
   case (snoc x xs)
@@ -81,7 +169,7 @@ next
                                 and A_in: "A \<in> M" by blast
   from A_der have "P \<turnstile> [Nt A] \<Rightarrow>* map Tm xs @ [Tm x,Nt B]" by simp
   with snoc.prems(2) obtain C where C_der: "P \<turnstile> [Nt A] \<Rightarrow>* map Tm xs @ [Nt C]"
-                                and C_prods: "(C,[Tm x, Nt B]) \<in> P" using rlin2_intro_tm by fast
+                                and C_prods: "(C,[Tm x, Nt B]) \<in> P" using rlin2_introduce_tm[of P A xs x B] by fast
   from A_in C_der snoc.prems(2) have "C \<in> foldl (nxt_rlin2_set P) M xs"
     using snoc.IH[of C] by auto
   with C_prods show ?case
@@ -92,7 +180,26 @@ lemma rlin2_tms_eps:
   assumes "rlin2 P"
       and "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w"
     shows "\<exists>B. P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt B] \<and> (B,[]) \<in> P"
-  sorry
+proof -
+  from assms(2) have "\<exists>v. P \<turnstile> [Nt A] \<Rightarrow>* v \<and> P \<turnstile> v \<Rightarrow> map Tm w"
+    using rtranclp.cases by force
+  then obtain v where v_star: "P \<turnstile> [Nt A] \<Rightarrow>* v" and v_step: "P \<turnstile> v \<Rightarrow> map Tm w" by blast
+  from v_step have "\<exists>u1 u2 C \<alpha>. v = u1 @ [Nt C] @ u2 \<and> map Tm w = u1 @ \<alpha> @ u2 \<and> (C,\<alpha>) \<in> P"
+    using derive.cases by fastforce
+  then obtain u1 u2 C \<alpha> where v_def: "v = u1 @ [Nt C] @ u2" and w_def: "map Tm w = u1 @ \<alpha> @ u2" and C_prod: "(C,\<alpha>) \<in> P" by blast
+  have "\<nexists>A. Nt A \<in> set (map Tm w)" by auto
+  with w_def have "\<nexists>A. Nt A \<in> set \<alpha>" 
+    by (metis Un_iff set_append)
+  then have "\<nexists>a A. \<alpha> = [Tm a, Nt A]" by auto
+  with assms(1) C_prod have alpha_eps: "\<alpha> = []"
+    using rlin2_def by force
+  from v_star assms(1) v_def have u2_eps: "u2 = []"
+    using rlin2_derive[of P "[Nt A]"] by simp
+  from w_def alpha_eps u2_eps have u1_def: "u1 = map Tm w" by simp
+  from v_star v_def u1_def u2_eps have 1: "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt C]" by simp
+  from alpha_eps C_prod have 2: "(C,[]) \<in> P"  by simp
+  from 1 2 show ?thesis by auto
+qed
 
 theorem accepted_if_Lang:
 assumes "rlin2 P"
@@ -100,7 +207,7 @@ shows "w \<in> Lang P A \<Longrightarrow> A \<in> M \<Longrightarrow> \<exists>Z
 proof -
   assume w_lang: "w \<in> Lang P A" and A_in: "A \<in> M"
   from assms w_lang obtain B where A_der: "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w @ [Nt B]" and B_in: "(B,[]) \<in> P" 
-    unfolding Lang_def using rlin2_tms_eps by fast
+    unfolding Lang_def using rlin2_tms_eps[of P A w] by auto
   from A_in A_der have "B \<in> nxts_rlin2_set P M w" 
     using mult_derive_to_nxts[OF assms] by auto
   with B_in show ?thesis by blast

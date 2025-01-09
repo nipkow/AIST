4d0f5930642375c68cfc0957ca152e9e397618a3
Jamie <jamiechen@tutanota.com>
2025-01-09 01:10:02 +0100

    Parts of the closure proofs

--- Chen/Unit.thy
+++ Chen/Unit.thy
@@ -10,7 +10,7 @@ hide_const (open) Topological_Spaces.closed
 text \<open>Unit closure\<close>
 
 definition unit_subst :: "('a, 'b) Prods \<Rightarrow> ('a, 'b) Prods" where
-"unit_subst P = {(A, \<alpha>) | A \<alpha>. \<exists> B. (A,[Nt B]) \<in> P \<and> (B,\<alpha>) \<in> P}"
+"unit_subst P = {(A, \<alpha>). \<exists> B. (A,[Nt B]) \<in> P \<and> (B,\<alpha>) \<in> P}"
 
 definition "unit_closure P = closure unit_subst P"
 
@@ -24,9 +24,29 @@ unfolding unit_closure_def by (simp add: closure_incr)
 lemma Lang_unit_closure_incr: "Lang P A \<subseteq> Lang (unit_closure P) A"
 by (simp add: unit_closure_incr Lang_mono)
 
+lemma derivs_if_derives_unit_closure_one_step: "(A,w) \<in> unit_closure P \<Longrightarrow> P \<turnstile> [Nt A] \<Rightarrow>* w"
+unfolding unit_closure_def proof (induction "(A,w)" arbitrary: A w rule: closure_induct)
+  case mono
+  then show ?case using mono_unit_subst by simp
+next
+  case base
+  then show ?case by (simp add: derives_Cons_rule)
+next
+  case (step A')
+  then have "(A,w) \<in> unit_subst A'"
+    by simp
+  then obtain B where "(A,[Nt B]) \<in> A'" and "(B,w) \<in> A'"
+    unfolding unit_subst_def by auto
+  then have "P \<turnstile> [Nt A] \<Rightarrow>* [Nt B]"
+    using step.hyps(1) by auto
+  moreover have "P \<turnstile> [Nt B] \<Rightarrow>* w"
+    using \<open>(B, w) \<in> A'\<close> step.hyps by auto
+  ultimately show ?case 
+    by simp
+qed
+
 (* This direction is slightly more interesting *)
 theorem derivs_if_derives_unit_closure: "unit_closure P \<turnstile> \<alpha> \<Rightarrow>* \<beta> \<Longrightarrow> P \<turnstile> \<alpha> \<Rightarrow>* \<beta>"
-(* should work *)
 proof (induction rule: rtrancl_derive_induct)
   case base
   then show ?case
@@ -34,7 +54,8 @@ proof (induction rule: rtrancl_derive_induct)
 next
   case (step u A v w)
   then show ?case
-    sorry
+    using derivs_if_derives_unit_closure_one_step
+    by (metis derives_append derives_prepend rtranclp_trans)
 qed
 
 lemma Lang_unit_closure_subset: "Lang (unit_closure P) A \<subseteq> Lang P A"
@@ -49,13 +70,52 @@ lemma closed_unit_subst_unit_closure: "closed unit_subst (unit_closure P)"
 
 definition "rm_unit P = {p \<in> unit_closure P .\<forall>B. snd p \<noteq> [Nt B]}"
 
-  text \<open>The main theorem:\<close>
-  theorem "Lang (rm_unit P) A = Lang P A"
-  proof
-      show "Lang (rm_unit P) A \<subseteq> Lang P A"
-        by (metis (mono_tags) Collect_subset Lang_mono Lang_unit_closure rm_unit_def)
-  next
-      show "Lang P A \<subseteq> Lang (rm_unit P) A"
-      sorry
+definition "unit w = (\<exists>B. w = [Nt B])"
+
+(* as written, this lemma does not hold, but a modified version of it might
+be helpful for proving the subsequent lemma*)
+lemma derivs_rm_unit_if_derives_unit_closure_one_step: 
+  "(A,w) \<in> unit_closure P \<Longrightarrow> rm_unit P \<turnstile> [Nt A] \<Rightarrow>* w"
+unfolding unit_closure_def proof (induction "(A,w)" rule: closure_induct)
+  case mono
+  then show ?case using mono_unit_subst by simp
+next
+  case base
+  then show ?case
+  oops
+
+(* I think this should be provable *)
+lemma derivs_rm_unit_if_derives_unit_closure: 
+  "unit_closure P \<turnstile> \<alpha> \<Rightarrow>* map Tm \<beta> \<Longrightarrow> rm_unit P \<turnstile> \<alpha> \<Rightarrow>* map Tm \<beta>"
+proof (induction rule: derives_induct')
+  case base
+  then show ?case by simp
+next
+  case (step u A v w)
+  then show ?case 
+  sorry
+  qed
+
+text \<open>The main theorem:\<close>
+theorem "Lang (rm_unit P) A = Lang P A"
+proof
+  show "Lang (rm_unit P) A \<subseteq> Lang P A"
+    by (metis (mono_tags) Collect_subset Lang_mono Lang_unit_closure rm_unit_def)
+next
+  have "\<And> w. w \<in> Lang (unit_closure P) A \<Longrightarrow> w \<in> Lang (rm_unit P) A"
+    proof -
+      fix w
+      assume "w \<in> Lang (unit_closure P) A"
+      then have "unit_closure P \<turnstile> [Nt A] \<Rightarrow>* map Tm w"
+        unfolding Lang_def by auto
+      then have "rm_unit P \<turnstile> [Nt A] \<Rightarrow>* map Tm w"
+        using derivs_rm_unit_if_derives_unit_closure by auto
+      then show "w \<in> Lang (rm_unit P) A"
+      unfolding Lang_def by simp
+    qed
+  then have "Lang (unit_closure P) A \<subseteq> Lang (rm_unit P) A"
+    by auto
+  then show "Lang P A \<subseteq> Lang (rm_unit P) A"
+    using Lang_unit_closure by fastforce
 qed
 end
--- Chen/Unproductive.thy
+++ Chen/Unproductive.thy
@@ -9,38 +9,76 @@ hide_const (open) Topological_Spaces.closed
 
 text \<open>Unproductive Symbol Elimination\<close>
 
-definition "rm_unproductive P = {(A,\<alpha>) |A \<alpha>. (A,\<alpha>) \<in> P \<and> Lang P A \<noteq> {}}"
+(* I think the inductive set definition is better, because we may want to use
+producthive.induct at some point *)
+inductive_set productive :: "('n,'t)Prods \<Rightarrow> 'n set" for P where
+"\<lbrakk> (A,\<alpha>) \<in> P; \<And>B. B \<in> nts_of_syms \<alpha> \<Longrightarrow> B \<in> productive P \<rbrakk> \<Longrightarrow> A \<in> productive P"
+print_theorems
 
-
-lemma prefix_rm_unproductive: "rm_unproductive P \<subseteq> P"
-  unfolding rm_unproductive_def by auto
-
-lemma derivs_if_derives_unproductive: "P \<turnstile> \<alpha> \<Rightarrow>* \<beta> \<Longrightarrow> rm_unproductive P \<turnstile> \<alpha> \<Rightarrow>* \<beta>"
-proof (induction rule: rtrancl_derive_induct)
-  case base
-  then show ?case by simp
-next
-  case (step u A v w)
-  then show ?case
-    oops
+definition "rm_unproductive P = {(A,\<alpha>) \<in> P. A \<in> productive P}"
 
 
 theorem "Lang (rm_unproductive P) A = Lang P A"
   proof
-    show "Lang (rm_unproductive P) A \<subseteq> Lang P A"
-      using prefix_rm_unproductive Lang_mono by metis
+    have "rm_unproductive P \<subseteq> P"
+      unfolding rm_unproductive_def by auto
+    then show "Lang (rm_unproductive P) A \<subseteq> Lang P A"
+      using Lang_mono by metis
   next
-    consider (LangEmpty) "Lang P A = {}" | (NotLangEmpty) "Lang P A \<noteq> {}"
-      by auto
-    then show "Lang P A \<subseteq> Lang (rm_unproductive P) A"
-    proof (cases)
-      case LangEmpty
-      then show ?thesis by simp
-    next
-      case NotLangEmpty
-      then show ?thesis
-      unfolding rm_unproductive_def
-      sorry
+    show "Lang P A \<subseteq> Lang (rm_unproductive P) A"
+      proof (cases)
+        assume "A \<in> productive P"
+        have "\<And> w. w \<in> Lang P A \<Longrightarrow> w \<in> Lang (rm_unproductive P) A"
+          proof -
+            fix w
+            assume "w \<in> Lang P A"
+            then have "P \<turnstile> [Nt A] \<Rightarrow>* map Tm w"
+              unfolding Lang_def by simp
+            then have "rm_unproductive P \<turnstile> [Nt A] \<Rightarrow>* map Tm w"
+            sorry
+
+            then show "w \<in> Lang (rm_unproductive P) A"
+            sorry
+          qed
+        then show ?thesis
+          by auto
+      next
+        assume "A \<notin> productive P"
+        have "Lang P A = {}"
+        proof (rule ccontr)
+          assume "Lang P A \<noteq> {}"
+          then obtain w where "P \<turnstile> [Nt A] \<Rightarrow>* w" and "nts_of_syms w = {}"
+            unfolding Lang_def nts_of_syms_def by fastforce
+
+          (* should follow frow A \<notin> productive P, although I'm not sure if the 
+          current attempt at proving it is particularly fruitful*)
+          moreover have "nts_of_syms w \<noteq> {}"
+            using \<open>P \<turnstile> [Nt A] \<Rightarrow>* w\<close> unfolding nts_of_syms_def 
+            proof (induction)
+              case base
+              then show ?case by simp
+            next
+              case (step y z)
+              then obtain \<alpha> \<beta> \<gamma> A' where "y = \<alpha> @ [Nt A'] @ \<gamma>" and "z = \<alpha> @ \<beta> @ \<gamma>"
+                by (meson derive.cases)
+              (* easily proved with an additional lemma*)
+              then have "A' \<notin> productive P"
+              using \<open>A \<notin> productive P\<close> step.hyps unfolding productive_def 
+              sorry
+              show ?case
+                proof (rule ccontr)
+                  assume "\<not>({A. Nt A \<in> set z} \<noteq> {})"
+                  (* then have "y \<in> productive P" *)
+                  then show False
+                sorry
+              qed
+            qed
+          
+          ultimately show False
+            by simp
+        qed
+        then show ?thesis
+          by auto
     qed
 qed
 end

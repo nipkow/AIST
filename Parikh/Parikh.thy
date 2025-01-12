812e2d7804c578029997eaf4d7100a88be671ff7
Fabian <fabian.lehr@tum.de>
2025-01-12 20:50:43 +0100

    finish proof of lemma

--- Parikh/Parikh.thy
+++ Parikh/Parikh.thy
@@ -7,6 +7,22 @@ theory Parikh
 begin
 
 
+section \<open>General stuff\<close>
+
+lemma langpow_mono:
+  fixes A :: "'a lang"
+  assumes "A \<subseteq> B"
+  shows "A ^^ n \<subseteq> B ^^ n"
+using assms conc_mono[of A B] by (induction n) auto
+
+
+lemma countable_union_finally_empty:
+  assumes "\<forall>j>i. f j = {}"
+  shows "(\<Union>j. f j) = (\<Union>j\<le>i. f j)"
+  sorry
+
+
+
 section \<open>functions of languages\<close>
 
 datatype 'a lfun = V nat
@@ -55,7 +71,6 @@ next
     by (smt (verit, best) eval.simps(6) in_star_iff_concat order_trans subsetI)
 qed auto
 
-
 lemma lfun_mono:
   fixes f :: "'a lfun"
   shows "mono (eval f)"
@@ -66,6 +81,83 @@ lemma substitution_lemma:
   assumes "\<forall>i. s' i = eval (upd i) s"
   shows "eval (subst f upd) s = eval f s'"
   using assms by (induction rule: lfun.induct) auto
+
+
+lemma vars_subst: "vars (subst f upd) = (\<Union>x \<in> vars f. vars (upd x))"
+  by (induction f) auto
+
+lemma vars_subst_upper: "vars (subst f upd) \<subseteq> (\<Union>x. vars (upd x))"
+  using vars_subst by force
+
+
+(* Continuity of lfun *)
+
+lemma lfun_cont_aux1:
+  assumes "\<forall>i. s i \<le> s (Suc i)"
+      and "w \<in> (\<Union>i. eval f (s i))"
+    shows "w \<in> eval f (\<lambda>x. \<Union>i. s i x)"
+proof -
+  from assms(2) obtain n where n_intro: "w \<in> eval f (s n)" by auto
+  have "s n x \<subseteq> (\<Union>i. s i x)" for x by auto
+  with n_intro show "?thesis"
+    using lfun_mono_aux[of "s n" "\<lambda>x. \<Union>i. s i x"] by auto
+qed
+
+lemma langpow_Union_eval:
+  assumes "\<forall>i. s i \<le> s (Suc i)"
+      and "w \<in> (\<Union>i. eval f (s i)) ^^ n"
+    shows "w \<in> (\<Union>i. eval f (s i) ^^ n)"
+using assms proof (induction n arbitrary: w)
+  case 0
+  then show ?case by simp
+next
+  case (Suc n)
+  then obtain u v where w_decomp: "w = u@v" and
+    "u \<in> (\<Union>i. eval f (s i)) \<and> v \<in> (\<Union>i. eval f (s i)) ^^ n" by fastforce
+  with Suc have "u \<in> (\<Union>i. eval f (s i)) \<and> v \<in> (\<Union>i. eval f (s i) ^^ n)" by auto
+  then obtain i j where i_intro: "u \<in> eval f (s i)" and j_intro: "v \<in> eval f (s j) ^^ n" by blast
+  let ?m = "max i j"
+  from i_intro Suc.prems(1) lfun_mono_aux have 1: "u \<in> eval f (s ?m)"
+    by (metis le_fun_def lift_Suc_mono_le max.cobounded1 subset_eq)
+  from Suc.prems(1) lfun_mono_aux have "eval f (s j) \<subseteq> eval f (s ?m)"
+    by (metis le_fun_def lift_Suc_mono_le max.cobounded2)
+  with j_intro langpow_mono have 2: "v \<in> eval f (s ?m) ^^ n" by auto
+  from 1 2 show ?case using w_decomp by auto
+qed
+
+lemma lfun_cont_aux2:
+  assumes "\<forall>i. s i \<le> s (Suc i)"
+      and "w \<in> eval f (\<lambda>x. \<Union>i. s i x)"
+    shows "w \<in> (\<Union>i. eval f (s i))"
+using assms proof (induction arbitrary: w rule: lfun.induct)
+  case (Conc f g)
+  then obtain u v where w_decomp: "w = u@v"
+    and "u \<in> eval f (\<lambda>x. \<Union>i. s i x) \<and> v \<in> eval g (\<lambda>x. \<Union>i. s i x)" by auto
+  with Conc have "u \<in> (\<Union>i. eval f (s i)) \<and> v \<in> (\<Union>i. eval g (s i))" by auto
+  then obtain i j where i_intro: "u \<in> eval f (s i)" and j_intro: "v \<in> eval g (s j)" by blast
+  let ?m = "max i j"
+  from i_intro Conc.prems(1) lfun_mono_aux have "u \<in> eval f (s ?m)"
+    by (metis le_fun_def lift_Suc_mono_le max.cobounded1 subset_eq)
+  moreover from j_intro Conc.prems(1) lfun_mono_aux have "v \<in> eval g (s ?m)"
+    by (metis le_fun_def lift_Suc_mono_le max.cobounded2 subset_eq)
+  ultimately show ?case using w_decomp by auto
+next
+  case (Star f)
+  then obtain n where n_intro: "w \<in> (eval f (\<lambda>x. \<Union>i. s i x)) ^^ n"
+    using eval.simps(6) star_pow by blast
+  with Star have "w \<in> (\<Union>i. eval f (s i)) ^^ n" using langpow_mono by blast
+  with Star.prems have "w \<in> (\<Union>i. eval f (s i) ^^ n)" using langpow_Union_eval by auto
+  then show ?case by (auto simp add: star_def)
+qed fastforce+
+
+lemma lfun_cont:
+  assumes "\<forall>i. s i \<le> s (Suc i)"
+  shows "eval f (\<lambda>x. \<Union>i. s i x) = (\<Union>i. eval f (s i))"
+proof
+  from assms show "eval f (\<lambda>x. \<Union>i. s i x) \<subseteq> (\<Union>i. eval f (s i))" using lfun_cont_aux2 by auto
+  from assms show "(\<Union>i. eval f (s i)) \<subseteq> eval f (\<lambda>x. \<Union>i. s i x)" using lfun_cont_aux1 by blast
+qed
+
 
 
 section \<open>Regular functions\<close>
@@ -82,12 +174,6 @@ inductive_cases ConstE [elim]:       "regular_fun (N l)"
 inductive_cases Union2E [elim]:      "regular_fun (Union2 f g)"
 inductive_cases ConcE [elim]:        "regular_fun (Conc f g)"
 inductive_cases StarE [elim]:        "regular_fun (Star f)"
-
-
-lemma countable_union_finally_empty:
-  assumes "\<forall>j>i. f j = {}"
-  shows "(\<Union>j. f j) = (\<Union>j\<le>i. f j)"
-  sorry
 
 
 lemma finite_union_regular:
@@ -133,6 +219,7 @@ next
 qed auto
 
 
+
 section \<open>Parikh image\<close>
 
 definition parikh_vec :: "'t list \<Rightarrow> ('t \<Rightarrow> nat)" where
@@ -146,6 +233,7 @@ definition subseteq_comm :: "'t lang \<Rightarrow> 't lang \<Rightarrow> bool" w
   "subseteq_comm L1 L2 \<equiv> parikh_img L1 \<subseteq> parikh_img L2"
 
 
+
 section \<open>systems of equations\<close>
 
 type_synonym 'a eq_sys = "'a lfun list"
@@ -154,6 +242,7 @@ type_synonym 'a eq_sys = "'a lfun list"
 definition indep_ub :: "'a eq_sys \<Rightarrow> nat \<Rightarrow> bool" where
   "indep_ub sys n \<equiv> \<forall>eq \<in> set sys. \<forall>x \<in> vars eq. x > n"
 
+(* sys independent on variables \<ge> n *)
 definition indep_lb :: "'a eq_sys \<Rightarrow> nat \<Rightarrow> bool" where
   "indep_lb sys n \<equiv> \<forall>eq \<in> set sys. \<forall>x \<in> vars eq. x < n"
 
@@ -177,17 +266,20 @@ definition partial_sol :: "'a eq_sys \<Rightarrow> 'a eq_sys \<Rightarrow> bool"
                   \<and> (\<forall>s. solves_eq_sys_comm (sys_subst sys (\<lambda>i. if i < length sys then sol ! i else V i)) s)"
 
 
+
 section \<open>The lemma from the paper\<close>
 
 fun g_pre :: "'a eq_sys \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> 'a lfun" where
   "g_pre _ _ 0 = N {}" |
   "g_pre f_sys i (Suc n) = subst (f_sys ! i) (\<lambda>j. if j < length f_sys then g_pre f_sys j n else V j)"
 
+
 lemma g_pre_eval:
   assumes "\<forall>j < length f_sys. s' j = eval (g_pre f_sys j n) s"
       and "\<forall>j \<ge> length f_sys. s' j = s j"
     shows "eval (g_pre f_sys i (Suc n)) s = eval (f_sys ! i) s'"
 using assms by (simp add: substitution_lemma)
+
 
 lemma g_pre_monotonically_increasing:
   "eval (g_pre f_sys i n) s \<subseteq> eval (g_pre f_sys i (Suc n)) s"
@@ -205,6 +297,7 @@ next
   also have "\<dots> = eval (g_pre f_sys i (Suc (Suc n))) s" using g_pre_eval[of f_sys ?s_Suc] by auto
   finally show ?case .
 qed
+
 
 lemma g_pre_subseteq_sol:
   assumes "i < length f_sys"
@@ -232,8 +325,30 @@ next
 qed
 
 
+lemma g_pre_indep:
+  assumes "i < length f_sys"
+  shows "\<forall>x \<in> vars (g_pre f_sys i n). x \<ge> length f_sys"
+proof (induction n arbitrary: i)
+  case 0
+  then show ?case by auto
+next
+  case (Suc n)
+  let ?upd = "\<lambda>x. if x < length f_sys then g_pre f_sys x n else V x"
+  from vars_subst_upper have "vars (g_pre f_sys i (Suc n)) \<subseteq> (\<Union>x. vars (?upd x))" by simp
+  moreover have "\<forall>y \<in> vars (?upd x). y \<ge> length f_sys" for x using Suc by simp
+  ultimately show ?case by auto
+qed
+
+
 definition g :: "'a eq_sys \<Rightarrow> nat \<Rightarrow> 'a lfun" where
   "g f_sys i \<equiv> UnionC (\<lambda>n. g_pre f_sys i n)"
+
+
+lemma g_indep:
+  assumes "i < length f_sys"
+  shows "\<forall>x \<in> vars (g f_sys i). x \<ge> length f_sys"
+unfolding g_def using assms g_pre_indep by auto
+
 
 lemma solves_g_if_solves_f_ineq:
   assumes "i < length f_sys"
@@ -244,6 +359,7 @@ unfolding g_def proof
   assume "x \<in> eval (UnionC (g_pre f_sys i)) s"
   then show "x \<in> eval (f_sys ! i) s" using g_pre_subseteq_sol[OF assms] by auto
 qed
+
 
 lemma Union_index_shift: "(\<Union>n. f n) = f 0 \<union> (\<Union>n. f (Suc n))"
 proof
@@ -266,7 +382,7 @@ proof
   show "f 0 \<union> (\<Union>n. f (Suc n)) \<subseteq> \<Union> (range f)" by auto
 qed
 
-lemma "(\<Union>n. eval (g_pre f_sys i (Suc n)) s)
+(*lemma "(\<Union>n. eval (g_pre f_sys i (Suc n)) s)
     \<subseteq> eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
 proof
   fix x
@@ -283,14 +399,32 @@ proof
     using lfun_mono_aux[of ?s_g_pre ?s_g] by auto
   then show "x \<in> eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
     using substitution_lemma by (smt (verit) eval.simps(1))
-qed
+qed*)
 
-lemma "eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s
-    \<subseteq> (\<Union>n. eval (g_pre f_sys i (Suc n)) s)"
-proof
-  fix x
-  assume "x \<in> eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
-  show "x \<in> (\<Union>n. eval (g_pre f_sys i (Suc n)) s)" sorry
+lemma g_pre_g_Union: "(\<Union>n. eval (g_pre f_sys i (Suc n)) s) =
+  eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
+proof -
+  let ?s = "(\<lambda>n j. if j < length f_sys then eval (g_pre f_sys j n) s else s j)"
+  have "?s n j \<subseteq> ?s (Suc n) j" for n j
+    using g_pre_monotonically_increasing by fastforce
+  then have s_monotone: "\<forall>n. ?s n \<le> ?s (Suc n)" by (simp add: le_fun_def)
+
+  have s_Union: "(\<Union>i. if x < length f_sys then eval (g_pre f_sys x i) s else s x) =
+    (if x < length f_sys then eval (g f_sys x) s else s x)" for x
+    unfolding g_def by simp
+
+  have "eval (g_pre f_sys i (Suc n)) s = eval (f_sys ! i) (?s n)" for n
+    using g_pre_eval[of f_sys] by auto
+  then have "(\<Union>n. eval (g_pre f_sys i (Suc n)) s) = (\<Union>n. eval (f_sys ! i) (?s n))" by auto
+  also have "\<dots> = eval (f_sys ! i) (\<lambda>x. \<Union>i. if x < length f_sys then eval (g_pre f_sys x i) s else s x)"
+    using s_monotone lfun_cont[of ?s "f_sys ! i"] by argo
+  also have "\<dots> = eval (f_sys ! i) (\<lambda>x. if x < length f_sys then eval (g f_sys x) s else s x)"
+    using s_Union by simp
+  also have "\<dots> = eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
+    using substitution_lemma[of "\<lambda>x. if x < length f_sys then eval (g f_sys x) s else s x"
+                                "\<lambda>i. if i < length f_sys then g f_sys i else V i"]
+    by fastforce
+  finally show ?thesis .
 qed
 
 lemma solves_f_if_solves_g_eq:
@@ -302,18 +436,34 @@ unfolding solves_eq_sys_def proof (standard, standard)
   with assms(1) have "s i = (\<Union>n. eval (g_pre f_sys i n) s)" unfolding g_def by auto
   also have "\<dots> = (\<Union>n. eval (g_pre f_sys i (Suc n)) s)"
     using Union_index_shift[of "\<lambda>n. eval (g_pre f_sys i n) s"] by auto
-  also have "\<dots> = eval (subst (f_sys ! i) (\<lambda>i. if i < length f_sys then g f_sys i else V i)) s"
-    sorry
-  also have "\<dots> = eval (f_sys ! i) s" using assms by (simp add: substitution_lemma)
+  also have "\<dots> = eval (f_sys ! i) s"
+    using g_pre_g_Union[of f_sys] assms by (simp add: substitution_lemma)
   finally show "eval (f_sys ! i) s = s i" by auto
 qed
+
 
 lemma lemma_paper:
   assumes "\<forall>eq \<in> set f_sys. regular_fun eq"
     shows "\<exists>g_sys. length g_sys = length f_sys \<and> indep_ub g_sys (length f_sys - 1)
-                \<and> (solves_ineq_sys f_sys s \<longrightarrow> solves_ineq_sys g_sys s)
-                \<and> (solves_eq_sys g_sys s \<longrightarrow> solves_eq_sys f_sys s)"
-  sorry
+                \<and> (\<forall>s. solves_ineq_sys f_sys s \<longrightarrow> solves_ineq_sys g_sys s)
+                \<and> (\<forall>s. solves_eq_sys g_sys s \<longrightarrow> solves_eq_sys f_sys s)"
+proof -
+  let ?g_sys = "map (\<lambda>i. g f_sys i) [0..<length f_sys]"
+  have length_g_sys: "length ?g_sys = length f_sys" by auto
+  have indep_g_sys: "indep_ub ?g_sys (length f_sys - 1)"
+    unfolding indep_ub_def using g_indep by fastforce
+
+  have "\<lbrakk> i < length f_sys; solves_ineq_sys f_sys s \<rbrakk> \<Longrightarrow> eval (?g_sys ! i) s \<subseteq> s i" for s i
+    using solves_g_if_solves_f_ineq solves_ineq_sys_def by fastforce
+  then have g_sol_of_f: "solves_ineq_sys f_sys s \<longrightarrow> solves_ineq_sys ?g_sys s" for s
+    using solves_ineq_sys_def by (metis length_g_sys)
+
+  have f_sol_of_g: "solves_eq_sys ?g_sys s \<longrightarrow> solves_eq_sys f_sys s" for s
+    using solves_f_if_solves_g_eq solves_eq_sys_def
+    by (metis add_0 diff_zero length_g_sys nth_map_upt)
+
+  from length_g_sys indep_g_sys g_sol_of_f f_sol_of_g show ?thesis by blast
+qed
 
 
 section \<open>The theorem from the paper\<close>

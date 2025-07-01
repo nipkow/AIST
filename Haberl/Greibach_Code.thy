afaa9863362297cd7a2e7038fc280ae829bfea25
nipkow <nipkow@in.tum.de>
2025-07-01 09:02:48 +0200

    top-level gnf_hd verified

--- Haberl/Greibach_Code.thy
+++ Haberl/Greibach_Code.thy
@@ -28,55 +28,72 @@ lemma distinct_nts_prods_list: "distinct(nts_prods_list ps)"
 by(induction ps) (auto simp: nts_prods_list_def distinct_nts_syms_list split: sym.split)
 (* end *)
 
-fun freshs :: "('a::fresh) set \<Rightarrow> 'a \<Rightarrow> nat \<Rightarrow> 'a list" where
-"freshs X a 0 = []" |
-"freshs X a (Suc n) = (let x = Fresh.fresh X a in x # freshs (insert x X) x n)"
+(* TODO: unify fresh and fresh0 *)
+class fresh0 = fresh +
+  fixes fresh0 :: "'a set \<Rightarrow> 'a"
+  assumes fresh0: "finite X \<Longrightarrow> fresh0 X \<notin> X"
+
+instantiation nat :: fresh0
+begin
+
+definition fresh0_nat :: "nat set \<Rightarrow> nat" where
+"fresh0_nat X \<equiv> fresh X 0"
+
+instance by standard (simp add: fresh0_nat_def fresh_notIn)
+
+end
+
+(* TODO integrate into the Fresh theories in the AFP *)
+fun freshs :: "('a::fresh0) set \<Rightarrow> nat \<Rightarrow> 'a list" where
+"freshs X 0 = []" |
+"freshs X (Suc n) = (let x = fresh0 X in x # freshs (insert x X) n)"
 
 declare freshs.simps(1)[code]
 
 lemma freshs_Suc_Code[code]:
-  "freshs (set xs) a (Suc n) = (let x = Fresh.fresh (set xs) a in x # freshs (set(x # xs)) x n)"
+  "freshs (set xs) (Suc n) = (let x = fresh0 (set xs) in x # freshs (set(x # xs)) n)"
 by simp
 
-value "freshs {1,3,5,7::nat} 3 4"
+value "freshs {0,1,3,5,7::nat} 4"
 
-lemma length_freshs: "finite X \<Longrightarrow> length(freshs X a n) = n"
+lemma length_freshs: "finite X \<Longrightarrow> length(freshs X n) = n"
 by(induction n arbitrary: X a)(auto simp: fresh_notIn Let_def)
 
-lemma freshs_disj: "finite X \<Longrightarrow> X \<inter> set(freshs X a n) = {}"
-proof(induction n arbitrary: X a)
+lemma freshs_disj: "finite X \<Longrightarrow> X \<inter> set(freshs X n) = {}"
+proof(induction n arbitrary: X)
   case (Suc n)
-  then show ?case by(simp add: fresh_notIn Let_def) blast
+  then show ?case by(simp add: fresh0 Let_def) blast
 qed simp
 
-lemma freshs_distinct: "finite X \<Longrightarrow> distinct (freshs X a n)"
-proof(induction n arbitrary: X a)
+lemma freshs_distinct: "finite X \<Longrightarrow> distinct (freshs X n)"
+proof(induction n arbitrary: X)
   case (Suc n)
   then show ?case
-    using freshs_disj[of "insert (Fresh.fresh X a) X" "Fresh.fresh X a" n]
-    by(auto simp: fresh_notIn Let_def)
+    using freshs_disj[of "insert (fresh0 X) X" n]
+    by(auto simp: fresh0 Let_def)
 qed simp
 
-lemma distinct_lem: "\<lbrakk> As = nts_prods_list ps; As' = freshs (set (A#As)) A (length As) \<rbrakk> \<Longrightarrow>
+lemma distinct_app_freshs: "\<lbrakk> As = nts_prods_list ps; As' = freshs (set As) (length As) \<rbrakk> \<Longrightarrow>
    distinct (As @ As')"
-using freshs_disj[of "insert A (set As)" A "length As"]
+using freshs_disj[of "set As" "length As"]
 by (auto simp: distinct_nts_prods_list freshs_distinct)
 
-lemma "\<lbrakk> As = nts_prods_list ps; As' = freshs (set (A#As)) A (length As) \<rbrakk> \<Longrightarrow>
-   Nts (set ps) \<inter> set As' = {}"
-  by (metis List.finite_set freshs_disj insert_disjoint(1) list.simps(15) set_nts_prods_list)
+text \<open>Top level function:\<close>
 
-lemma "\<lbrakk> As = nts_prods_list ps; As' = freshs (set (A#As)) A (length As) \<rbrakk> \<Longrightarrow>
-   A \<notin> set As'"
-  by (metis freshs_disj finite_set list.simps(15) insert_disjoint(2))
-
-text \<open>Top level function, not yet verified\<close>
-
-definition gnf_hd :: "('n::fresh,'t)prods \<Rightarrow> ('n,'t)Prods" where
+definition gnf_hd :: "('n::fresh0,'t)prods \<Rightarrow> ('n,'t)Prods" where
 "gnf_hd ps =
   (let As = nts_prods_list ps;
-       As' = freshs (set As) (hd As) (length As)
+       As' = freshs (set As) (length As)
    in exp_triangular (As' @ rev As) (solve_tri As As' (set ps)))"
+
+lemma GNF_hd_gnf_hd: "eps_free ps \<Longrightarrow> GNF_hd (gnf_hd ps)"
+by(simp add: gnf_hd_def Let_def GNF_of_R[simplified]
+  distinct_nts_prods_list freshs_distinct finite_nts freshs_disj set_nts_prods_list length_freshs)
+
+lemma Lang_gnf_hd: "\<lbrakk> eps_free ps; A \<in> nts ps \<rbrakk> \<Longrightarrow> Lang (gnf_hd ps) A = lang ps A"
+unfolding gnf_hd_def Let_def
+by (metis GNF_of_R_Lang IntI distinct_app_freshs empty_iff finite_nts freshs_disj length_freshs order_refl
+      set_nts_prods_list)
 
 
 text "Examples"

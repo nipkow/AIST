701951342fa5c2fc37890a6917a70110461a46a0
nipkow <nipkow@in.tum.de>
2025-10-10 11:52:17 +0900

    started Paper

---
+++ Paper/Paper.thy
@@ -0,0 +1,40 @@
+(*<*)
+theory Paper
+imports Context_Free_Grammar.Context_Free_Grammar
+begin
+(*>*)
+text \<open>
+\section{Introduction}
+
+The formalization is unified in the sense that some of the topics that were 
+previously formalized independently (and in different provers) are now
+unified in a single formalization.
+%rebase LL1 parser on CFG!
+
+Of course only textbook+, no parsing (separate).
+
+\subsection{Related Work}
+
+\subsection{Notation}
+
+\section{The Basic Theory}
+
+\subsection{Context-Free Grammars}
+\subsection{Chomsky Normal Form}
+\subsection{Pushdown Automata}
+\subsection{Pumping Lemma}
+\subsection{Automata}
+
+2DFA AFA?
+
+\section{Pre*}
+
+\section{Greibach}
+
+\section{Chomsky-Sch\"utzenberger}
+
+\section{Parikh}
+\<close>
+(*<*)
+end
+(*>*)
